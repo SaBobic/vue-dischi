4d2388ce6481d319fbab0837185eccff7ad61d99
SaBobic <fabrizioettori@gmail.com>
2022-07-26 15:19:37 +0200

    added MainHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-
+  <MainHeader />
 </template>
 
 <script>
+import MainHeader from './components/MainHeader.vue';
 
 export default {
   name: 'App',
   components: {
+    MainHeader,
   }
 }
 </script>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <nav class="d-flex align-items-center">
+            <img src="../assets/img/spotify-logo-png-7053.png" alt="Spotify Logo">
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'MainHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/vars';
+
+nav {
+    padding: 0 20px;
+    height: 70px;
+    background-color: $secondary_color;
+
+    img {
+        height: 50px;
+    }
+}
+</style>
